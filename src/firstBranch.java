6685bbe65682200855aa6d8e2dcaebc08c3e44c7
busabile <busabile@gmail.com>
2020-02-19 14:42:42 +0200

    A feature to add two numbers

---
+++ src/firstBranch.java
@@ -0,0 +1,13 @@
+
+public class firstBranch
+{
+    public static void main(String [] args) {
+
+        // Adding two numbers
+        int num1 = 5;
+        int num2 = 15;
+        int sum = sum = num1 + num2;
+
+        System.out.println("Sum of these numbers: " + sum);
+    }
+}
