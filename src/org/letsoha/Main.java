1225261f6a2a558507abd101dc1b231f179919fd
busabile <busabile@gmail.com>
2020-02-19 14:19:49 +0200

    Adding commentry to the code

--- src/org/letsoha/Main.java
+++ src/org/letsoha/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args)
     {
+        // First statement
 	    System.out.println("First program before GIT enabled");
+
+	    //Adding the body line
+	    System.out.println("This is the body");
     }
 }
